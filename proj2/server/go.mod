d6ac4a1e4360237220d17855e2bad921dff7e092
Alexey Sharov (Lazada Group) <alex.sharov@LAZ-VN-L-M-1099.local>
2018-09-10 16:05:01 +0800

    add micro

--- proj2/server/go.mod
+++ proj2/server/go.mod
@@ -1,3 +1,3 @@
 module github.com/nizsheanez/monorepo/proj2/server
 
-require github.com/nizsheanez/monorepo/proj1/client v1.0.1
+require github.com/nizsheanez/monorepo/proj1/client v1.0.2
--- proj2/server/main.go
+++ proj2/server/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	proj1_client "github.com/nizsheanez/monorepo/proj1/client/"
 )
 
 func main() {
-	fmt.Println(DeleteDuplicates([]int{1,2,3,4,1}))
-	proj1_client.Hello1()
+	proj1_client.NewApiClient()
 }
 
 
