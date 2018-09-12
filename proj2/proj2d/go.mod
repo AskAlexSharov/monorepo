d856b99857165f3edf61aff67b63421005126ae1
alex.sharov <www.pismeco@gmail.com>
2018-09-12 15:57:43 +0800

    clients now using todo_client package - so, importers will not need to setup alias

--- proj2/proj2d/go.mod
+++ proj2/proj2d/go.mod
@@ -2,8 +2,8 @@ module github.com/nizsheanez/monorepo/proj2/server
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway v1.5.0 // indirect
-	github.com/nizsheanez/monorepo/todo/client v1.1.12
-	github.com/nizsheanez/monorepo/todo/client/v2 v2.1.12
+	github.com/nizsheanez/monorepo/todo/sdk v1.2.1
+	github.com/nizsheanez/monorepo/todo/sdk/v2 v2.2.1
 	google.golang.org/genproto v0.0.0-20180831171423-11092d34479b // indirect
 	google.golang.org/grpc v1.15.0
 )
--- proj2/proj2d/main.go
+++ proj2/proj2d/main.go
@@ -11,8 +11,8 @@ import (
 	"time"
 
 	//"github.com/nizsheanez/monorepo/todo/projects"
-	todoV1 "github.com/nizsheanez/monorepo/todo/client"
-	"github.com/nizsheanez/monorepo/todo/client/v2"
+	todo_sdk_v1 "github.com/nizsheanez/monorepo/todo/sdk"
+	"github.com/nizsheanez/monorepo/todo/sdk/v2"
 )
 
 var (
@@ -28,13 +28,13 @@ func main() {
 	}
 	defer conn.Close()
 
-	todoService := todo.NewTodoServiceClient(conn)
-	todoServiceV1 := todoV1.NewTodoServiceClient(conn)
+	todoService := todo_sdk.NewTodoServiceClient(conn)
+	todoServiceV1 := todo_sdk_v1.NewTodoServiceClient(conn)
 
 	//projectsApi := projects.NewApiClient(conn)
 
-	fmt.Println(todoService.List(context.Background(), &todo.ListRequest{}))
-	fmt.Println(todoServiceV1.List(context.Background(), &todoV1.ListRequest{}))
+	fmt.Println(todoService.List(context.Background(), &todo_sdk.ListRequest{}))
+	fmt.Println(todoServiceV1.List(context.Background(), &todo_sdk_v1.ListRequest{}))
 
 	//projectsApi.List(context.Background(), &projects.ListRequest{})
 }
